bdb55aa9a0333f69d8f27c911c4cf093e281e3c4
Marcel Rivero <126285355+Marci25@users.noreply.github.com>
2024-06-14 16:58:18 +0000

    CardGenerator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+      generateCard()
+}
+
+  function generateCard(){
+    let simbols=["♦" , "♥" , "♠" , "♣"]
+    let numbers=[ "A" ,"2" ,"3" ,"4" ,"5" ,"6" ,"7" , "8", "9", "10", "J", "Q", "K"];
+
+    let indexSimbols = Math.floor(Math.random()* simbols.length);
+    let indexNumbers = Math.floor(Math.random()* numbers.length);
+
+    document.querySelector("#top").innerHTML = simbols[indexSimbols]
+    document.querySelector("#number").innerHTML = numbers[indexNumbers]
+    document.querySelector("#bottom").innerHTML = simbols[indexSimbols]
+
+    let colorSimbols = simbols[indexSimbols] == "♦" || simbols[indexSimbols] == "♥" ? "text-danger" : "text-dark";
+
+    document.querySelector("#top").className = colorSimbols;
+    document.querySelector("#number").className = colorSimbols;
+    document.querySelector("#bottom").className = colorSimbols;
+  }
+
+  document.querySelector("#start").addEventListener("click", generateCard);
+
